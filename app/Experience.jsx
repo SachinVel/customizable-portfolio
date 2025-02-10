c75b6de99792398638194882feaef8f8441ad4d9
sachinvel <velsachin98@gmail.com>
2025-02-09 21:21:22 -0700

    change experience UI

--- app/Experience.jsx
+++ app/Experience.jsx
@@ -1,52 +1,85 @@
 "use client";
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { Accordion, AccordionTab } from 'primereact/accordion';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faLocationDot, faCalendar, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Experience({ content }) {
 
-    const [selectedExperience, setSelectedExperience] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    useEffect(() => {
-        if (content.experienceList.length > 0) {
-            setSelectedExperience(content.experienceList[0]);
-        }
-    }, [content.experienceList]);
+    const accordionVariants = {
+        hidden: { opacity: 0, height: 0 },
+        visible: { opacity: 1, height: 'auto' }
+    };
 
     return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ width: '25%', borderRight: '1px solid #ccc', padding: '10px' }}>
-                {content.experienceList.map((exp, index) => (
-                    <div 
-                        key={index} 
-                        onMouseEnter={() => setSelectedExperience(exp)} 
-                        style={{ cursor: 'pointer', marginBottom: '10px' }}
-                    >
-                        <h4>{exp.position}</h4>
-                        <p>{exp.company}</p>
-                        <p>{exp.startDate} - {exp.endDate}</p>
-                    </div>
-                ))}
+        <div className='relative md:m-8 text-white'>
+            <div className="text-center text-3xl text-white font-bold my-[3%]">
+                {content.title}
             </div>
-            <div style={{ width: '75%', padding: '10px' }}>
-                {selectedExperience && (
-                    <>
-                        <h2>{selectedExperience.position}</h2>
-                        <h3>{selectedExperience.company}</h3>
-                        <p>{selectedExperience.startDate} - {selectedExperience.endDate}</p>
-                        <p>{selectedExperience.location}</p>
-                        <h4>Technologies:</h4>
-                        <ul>
-                            {selectedExperience.technologies.map((tech, index) => (
-                                <li key={index}>{tech}</li>
-                            ))}
-                        </ul>
-                        <h4>Description:</h4>
-                        <ul>
-                            {selectedExperience.description.map((desc, index) => (
-                                <li key={index}>{desc}</li>
-                            ))}
-                        </ul>
-                    </>
-                )}
+            <div className='flex justify-center md:px-10 px-8'>
+                <Accordion className="md:w-[70rem] w-[100rem]" activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
+                    {content.experienceList.map((experience, index) => (
+                        <AccordionTab key={index}
+                            header={
+                                <div className="flex rounded-md justify-between items-center bg-homebg text-black p-2 mb-2 w-full">
+                                    <div className="flex items-center">
+                                        <img src={experience.logo} alt="logo" className="w-10 h-10 mr-4" />
+                                        <div>
+                                            <strong>{experience.position} @ {experience.company}</strong>
+                                        </div>
+                                    </div>
+                                    <div className='flex items-center gap-5 '>
+                                        <strong className='hidden md:block'>{experience.startDate} - {experience.endDate}</strong>
+                                        <span className="accordion-icon">
+                                            {activeIndex === index ? <FontAwesomeIcon icon={faMinus} className='text-blue-500'/> : <FontAwesomeIcon icon={faPlus} className='text-blue-500'/>}
+                                        </span>
+                                    </div>
+                                </div>
+                            }
+                            headerClassName="p-accordion-header">
+                            <motion.div
+                                initial="hidden"
+                                animate="visible"
+                                variants={accordionVariants}
+                                transition={{ duration: 0.5 }}
+                                className='bg-gray-800 border border-white my-5 p-5 rounded-lg shadow-lg overflow-hidden'
+                            >
+                                <div className="flex block md:hidden items-center gap-2 mb-2">
+                                    <FontAwesomeIcon
+                                        icon={faCalendar}
+                                        className="text-blue-500 text-xl"
+                                    />
+                                    <p>{experience.startDate} - {experience.endDate}</p>
+                                </div>
+
+                                <div className="flex items-center gap-2 mb-2">
+                                    <FontAwesomeIcon
+                                        icon={faLocationDot}
+                                        className="text-blue-500 text-xl"
+                                    />
+                                    <p>{experience.location}</p>
+                                </div>
+                                <ul className="list-disc pl-5">
+                                    {experience.description.map((desc, index) => (
+                                        <li key={index}>{desc}</li>
+                                    ))}
+                                </ul>
+                                <div className="flex h-fit flex-wrap gap-2 mt-4">
+                                    {experience.keywords.map((keyword, index) => (
+                                        <span key={index} className="bg-blue-500 h-8 text-white flex items-center rounded-full px-3 py-1 text-sm">
+                                            {keyword}
+                                        </span>
+                                    ))}
+                                </div>
+                            </motion.div>
+                        </AccordionTab>
+                    ))}
+                </Accordion>
             </div>
         </div>
     );
