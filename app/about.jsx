00141cf54e0f05ecd2279315ac5ed00fd7aa9f12
sachinvel <velsachin98@gmail.com>
2025-02-09 21:20:54 -0700

    change about UI

--- app/about.jsx
+++ app/about.jsx
@@ -6,24 +6,20 @@ import { ContainerScroll } from "./components/ui/container-scroll-animation";
 const About = ({ content }) => {
   // use
   return (
-
-
     <ContainerScroll
-      className="bg-gradient-to-r from-blue-500 to-blue-700 my-0"
+      className="bg-gradient-to-r from-blue-500 to-blue-700"
       titleComponent={
-        <div className="text-center z-10 relative text-3xl text-white font-bold my-2">
+        <div className="text-center z-10 relative text-3xl text-white font-bold md:mb-10">
           {content.title}
         </div>
       }
     >
-
-      <section className="relative py-12 text-black px-6 md:px-24 border-1">
-
+      <section className="relative text-white px-6 md:px-24 border-1">
         {/* Tilted Background */}
         <div className="absolute inset-0 transform "></div>
-        
+
         {/* Content */}
-        <div className="max-w-4xl mx-auto  flex flex-col md:flex-row items-center gap-8">
+        <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-center gap-8">
           {/* Image Section */}
           <div className="flex-shrink-0">
             <div className="aspect-w-4 aspect-h-3 relative w-64 h-64 md:w-80 md:h-80 rounded-md overflow-hidden">
@@ -36,20 +32,14 @@ const About = ({ content }) => {
             </div>
           </div>
 
-          {/* Text Section */}
           <div className="text-center md:text-left z-20">
-            {/* <h2 className="text-2xl font-bold mb-4">{content.title}</h2> */}
             <p className="text-md leading-relaxed">
               {content.description}
             </p>
           </div>
         </div>
-
       </section>
-
     </ContainerScroll>
-
-
   );
 };
 
